27d563357e8e896ee2ed250d02418c0c928795c1
hyyc116 <hyyc116@gmail.com>
2017-03-07 15:57:47 -0500

    add

--- crawler/advence_search.py
+++ crawler/advence_search.py
@@ -115,4 +115,4 @@ def advance_search(query_input,save_dir,startYear=1900,endYear=2017,s=2,e=4):
     time.sleep(1)
 
 if __name__=="__main__":
-    advance_search('TS=({:})'.format(sys.argv[1]),sys.argv[2],int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5],sys.argv[6]))
+    advance_search('TS=({:})'.format(sys.argv[1]),sys.argv[2],int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]))
