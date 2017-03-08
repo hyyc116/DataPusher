29857c32af2eb4debd0dd74e2d74e5bce438a3b8
hyyc116 <hyyc116@gmail.com>
2017-03-07 21:04:40 -0500

    ref link

--- crawler/advence_search.py
+++ crawler/advence_search.py
@@ -109,9 +109,12 @@ def advance_search(query_input,save_dir,startYear=1900,endYear=2017,s=2,e=4):
 
             #get reference list
             soup = bs(html,'lxml')
-            snd_p= soup.select('div.block-text-content p')[2]
-            ref_link = snd_p.select('a[href]')
-            print ref_link
+            snd_p= soup.select('div.block-text-content p')[1]
+            refs= snd_p.select('a[href]')
+            if len(refs==1:
+                logging.info('found reference list')
+                ref_link = refs[0].href
+                print ref_link
             time.sleep(1)
         #To avoid Session expired, every 5 pages request for a new session
         if page%5 == 0:
