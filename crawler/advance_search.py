f82d1616356a618381a4c3afa2ec0c9cd7d135df
hyyc116 <hyyc116@gmail.com>
2017-03-08 15:49:34 -0500

    ref

--- crawler/advance_search.py
+++ crawler/advance_search.py
@@ -135,6 +135,7 @@ def iter_ref_pages(ref_url,handler,base_url):
     page_num=1
     html = handler.get_url_with_cookie(ref_url)
     links = handler.return_all_pages(html)
+    crawl_pages(links,handler,base_url,ref_url)
     open('test-ref-{:}.html'.format(page_num),'w').write(html)
     logging.info('Ref page {:} found {:} links'.format(page_num,len(links)))
     soup = bs(html,'lxml')
@@ -145,15 +146,21 @@ def iter_ref_pages(ref_url,handler,base_url):
         html = handler.get_url_with_cookie(next_url)
         open('test-ref-{:}.html'.format(page_num),'w').write(html)
         links = handler.return_all_pages(html)
-        for i,link in enumerate(links):
-            logging.info("dealing {:} reference paper.".format(i))
-            ref_paper_link=base_url+link
-            ref_html = handler.get_url_with_cookie(ref_paper_link)
-            open('ref_paper_{:}.html'.format(i),"w").write(ref_html)
+        crawl_pages(links,handler,base_url,next_url)
         logging.info('Ref page {:} found {:} links'.format(page_num,len(links)))
         soup = bs(html,'lxml')
         nextPage = soup.select('a.paginationNext')[0]
         next_url = nextPage.get('href')
+
+
+def crawl_pages(links,handler,base_url,ref_url):
+    handler.headers['Referer']=ref_url
+    for i,link in enumerate(links):
+        logging.info("dealing {:} reference paper.".format(i))
+        ref_paper_link=base_url+link
+        ref_html = handler.get_url_with_cookie(ref_paper_link)
+        open('ref_paper_{:}.html'.format(i),"w").write(ref_html)
+
 
 
 
